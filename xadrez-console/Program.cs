e2af5e38c1a62596776d511c9d64ee636c2be713
Diego Gomes <diegogns18@outlook.com>
2023-09-11 14:19:22 -0300

    Comecando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -5,3 +5,5 @@ Posicao P;
 P = new Posicao(3, 4);
 
 Console.WriteLine("Posicão: " + P);
+
+Tabuleiro tab = new Tabuleiro(8, 8);
---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class Peca
+    {
+        //Uma peça tem posicão, cor, movimentos e faz parte do tabuleiro
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QtdeMovimentos { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            QtdeMovimentos = 0;
+        }
+    }
+}
